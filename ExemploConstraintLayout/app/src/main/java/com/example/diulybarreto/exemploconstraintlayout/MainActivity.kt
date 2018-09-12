180b12c6724fdc6cec011c3a3a8991bdb4051c70
Diuly Barreto <diuly.barreto@cwi.com.br>
2018-09-12 11:26:29 -0300

    feat/ adicionar ação de botão de menu

--- ExemploConstraintLayout/app/src/main/java/com/example/diulybarreto/exemploconstraintlayout/MainActivity.kt
+++ ExemploConstraintLayout/app/src/main/java/com/example/diulybarreto/exemploconstraintlayout/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.diulybarreto.exemploconstraintlayout
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonMenu.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        addToCart.setOnClickListener {
+            Toast.makeText(this,
+                    "Added to Cart",
+                    Toast.LENGTH_SHORT).show()
+        }
     }
 }
